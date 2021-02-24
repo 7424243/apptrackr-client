5ed563467632baf3c2dbf1dbd77a6e0173ea0aff
Sam Sheets <sam.sheets94@gmail.com>
2021-02-23 17:29:03 -0700

    added trash icon to ResourcesList component

--- src/Components/ResourcesList/ResourcesList.js
+++ src/Components/ResourcesList/ResourcesList.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import ResourceItem from '../ResourceItem/ResourceItem'
 import SquareButton from '../SquareButton/SquareButton'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import './ResourcesList.css'
 
 class ResourcesList extends Component {
@@ -17,7 +19,7 @@ class ResourcesList extends Component {
                                     url={'https://www.indeed.com/'}
                                 />
                                 <SquareButton 
-                                    content={'x'}
+                                    content={<FontAwesomeIcon icon={faTrashAlt}/>}
                                     path={'/resources'}
                                 />
                             </div>
@@ -27,7 +29,7 @@ class ResourcesList extends Component {
                                     url={'https://www.linkedin.com/jobs/?trk=li_sem_namer_careers_jobsgtm_Careers_NAMER_T1_USCA_Search_Google-Brand_DR-PRS_Multiple_Brand-LinkedIn-Jobs-Exact_Desktop_English_Core_network=g_campaign=11602008719_keyword=linkedin%20jobs&src=go-pa&veh=Careers_NAMER_T1_USCA_Search_Google-Brand_DR-PRS_Multiple_Brand-LinkedIn-Jobs-Exact_Desktop_English_Core_479259659412_linkedin%20jobs_c__kwd-1528692947_11602008719&mcid=&cname=Careers_NAMER_T1_USCA_Search_Google-Brand_DR-PRS_Multiple_Brand-LinkedIn-Jobs-Exact_Desktop_English_Core&camid=11602008719&asid=113158397036&keyword=linkedin%20jobs&targetid=kwd-1528692947&crid=479259659412&placement=&dev=c&gclid=Cj0KCQiA7NKBBhDBARIsAHbXCB7OLVWs8pYmpW31PIvUl-AppU9qdI8QLsUKJMSsiLifTs41no_v3aYaAhwZEALw_wcB'}
                                 />
                                 <SquareButton 
-                                    content={'x'}
+                                    content={<FontAwesomeIcon icon={faTrashAlt}/>}
                                     path={'/resources'}
                                 />
                             </div>
@@ -37,7 +39,7 @@ class ResourcesList extends Component {
                                     url={'https://apprentice.at/'}
                                 />
                                 <SquareButton 
-                                    content={'x'}
+                                    content={<FontAwesomeIcon icon={faTrashAlt}/>}
                                     path={'/resources'}
                                 />
                             </div>
@@ -59,7 +61,7 @@ class ResourcesList extends Component {
                                     url={'https://resumekraft.com/wp-content/uploads/2019/12/teacher-resume-example.jpg'}
                                 />
                                 <SquareButton 
-                                    content={'x'}
+                                    content={<FontAwesomeIcon icon={faTrashAlt}/>}
                                     path={'/resources'}
                                 />
                             </div>
@@ -69,7 +71,7 @@ class ResourcesList extends Component {
                                     url={'https://www.thebalancecareers.com/thmb/IC37LV1es8xHWzy2TdJi5sFvIbg=/1650x1275/filters:no_upscale():max_bytes(150000):strip_icc()/The_Balance_Cover_2060250Aug-edddc2451f264a6eb1c94cf627cb77ff.jpg'}
                                 />
                                 <SquareButton 
-                                    content={'x'}
+                                    content={<FontAwesomeIcon icon={faTrashAlt}/>}
                                     path={'/resources'}
                                 />
                             </div>
@@ -79,7 +81,7 @@ class ResourcesList extends Component {
                                     url={'https://skillsyncer.com/'}
                                 />
                                 <SquareButton 
-                                    content={'x'}
+                                    content={<FontAwesomeIcon icon={faTrashAlt}/>}
                                     path={'/resources'}
                                 />
                             </div>
