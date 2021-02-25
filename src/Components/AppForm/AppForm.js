b15c7bddbcfb7b311e927624b07cba446e464064
Sam Sheets <sam.sheets94@gmail.com>
2021-02-25 11:48:33 -0700

    added basic responsiveness to AppForm component

--- src/Components/AppForm/AppForm.js
+++ src/Components/AppForm/AppForm.js
@@ -15,90 +15,89 @@ class AppForm extends Component {
             apps.find(app => app.id === appId)
         const appDetails = getApp(apps, appId)
         return (
-            <section>
-                <div>
-                    <h3>New Job Application</h3>
-                    <ErrorBoundary>
-                        <form>
-                            <section>
-                                <input 
-                                    name='job' 
-                                    defaultValue={appDetails ? appDetails.job_name : null} 
-                                    placeholder='Job Title'
-                                />
-                            </section>
-                            <section>
-                                <input 
-                                    name='company' 
-                                    defaultValue={appDetails ? appDetails.company_name : null} 
-                                    placeholder='Company Name'
-                                />
-                            </section>
-                            <section>
-                                <input 
-                                    name='website' 
-                                    defaultValue={appDetails ? appDetails.website : null} 
-                                    placeholder='Website'
-                                />
-                            </section>
-                            <section>
-                                <input 
-                                    name='date-applied' 
-                                    defaultValue={appDetails ? appDetails.date_applied : null} 
-                                    placeholder='Date Applied'
-                                />
-                            </section>
-                            <section>
-                                <input 
-                                    name='contact' 
-                                    defaultValue={appDetails ? appDetails.contact_name : null} 
-                                    placeholder='Contact Name'
-                                />
-                            </section>
-                            <section>
-                                <input 
-                                    name='contact_email' 
-                                    defaultValue={appDetails ? appDetails.contact_email : null} 
-                                    placeholder='Contact Email'
-                                />
-                            </section>
-                            <section>
-                                <input 
-                                    name='contact_phone' 
-                                    defaultValue={appDetails ? appDetails.contact_phone : null} 
-                                    placeholder='Contact Phone'
-                                />
-                            </section>
-                            <section>
-                                <input 
-                                    name='interview_date' 
-                                    defaultValue={appDetails ? appDetails.interview_date : null} 
-                                    placeholder='Interview Date'
-                                />
-                            </section>
-                            <section>
-                                <label htmlFor='status'>Status: </label>
-                                <select>
-                                    <option value='' disabled>Choose here</option>
-                                    <option value='Interested'>Interested</option>
-                                    <option value='Active'>Active</option>
-                                    <option value='Closed'>Closed</option>
-                                </select>
-                            </section>
-                            <section>
-                                <textarea 
-                                    defaultValue={appDetails ? appDetails.notes : null} 
-                                    placeholder='Additional Notes...'
-                                />
-                            </section>
-                        </form>
-                    </ErrorBoundary>
-                    <SquareButton
-                        content={<FontAwesomeIcon icon={faSave}/>}
-                        path={'/jobapps'}
-                    />
-                </div>
-            </section>
+            <div className='appform_container'>
+                <h2 className='appform_header'>New Job Application</h2>
+                <ErrorBoundary>
+                    <form className='appform_form'>
+                        <section className='appform_input'>
+                            <input 
+                                name='job' 
+                                defaultValue={appDetails ? appDetails.job_name : null} 
+                                placeholder='Job Title'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <input 
+                                name='company' 
+                                defaultValue={appDetails ? appDetails.company_name : null} 
+                                placeholder='Company Name'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <input 
+                                name='website' 
+                                defaultValue={appDetails ? appDetails.website : null} 
+                                placeholder='Website'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <input 
+                                name='date-applied' 
+                                defaultValue={appDetails ? appDetails.date_applied : null} 
+                                placeholder='Date Applied'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <input 
+                                name='contact' 
+                                defaultValue={appDetails ? appDetails.contact_name : null} 
+                                placeholder='Contact Name'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <input 
+                                name='contact_email' 
+                                defaultValue={appDetails ? appDetails.contact_email : null} 
+                                placeholder='Contact Email'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <input 
+                                name='contact_phone' 
+                                defaultValue={appDetails ? appDetails.contact_phone : null} 
+                                placeholder='Contact Phone'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <input 
+                                name='interview_date' 
+                                defaultValue={appDetails ? appDetails.interview_date : null} 
+                                placeholder='Interview Date'
+                            />
+                        </section>
+                        <section className='appform_input'>
+                            <label htmlFor='status'>Status: </label>
+                            <select>
+                                <option value='' disabled>Choose here</option>
+                                <option value='Interested'>Interested</option>
+                                <option value='Active'>Active</option>
+                                <option value='Closed'>Closed</option>
+                            </select>
+                        </section>
+                        <section className='appform_input appform_textarea'>
+                            <textarea 
+                                defaultValue={appDetails ? appDetails.notes : null} 
+                                placeholder='Additional Notes...'
+                            />
+                        </section>
+                            <SquareButton
+                            content={<FontAwesomeIcon icon={faSave}/>}
+                            path={'/jobapps'}
+                            />
+                    </form>
+                </ErrorBoundary>
+
+            </div>
         )
     }
 }
