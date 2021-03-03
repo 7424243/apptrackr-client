56678d8ea1f2f28aec457fad14ca74076a46f31e
Sam Sheets <sam.sheets94@gmail.com>
2021-03-03 11:26:52 -0700

    fixed failing tetst for NewResourceForm, AppForm, and ResourcesList components

--- src/Components/NewResourceForm/NewResourceForm.test.js
+++ src/Components/NewResourceForm/NewResourceForm.test.js
@@ -8,6 +8,7 @@ describe('NewResourceForm Component', () => {
 
     //smoke test
     it('renders without crashing', () => {
+        Storage.prototype.getItem = jest.fn(() => 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJpYXQiOjE2MTE5NTYwMDMsInN1YiI6ImRlbW8ifQ.f9WIVijmP-pud7JsTd2ysx4eqtpm6JnzOkUeGRihdVE')
         const div = document.createElement('div')
         ReactDOM.render(<BrowserRouter><NewResourceForm/></BrowserRouter>, div)
         ReactDOM.unmountComponentAtNode(div)
@@ -15,6 +16,7 @@ describe('NewResourceForm Component', () => {
 
     //snapshot test
     it('renders the UI as expected', () => {
+        Storage.prototype.getItem = jest.fn(() => 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJpYXQiOjE2MTE5NTYwMDMsInN1YiI6ImRlbW8ifQ.f9WIVijmP-pud7JsTd2ysx4eqtpm6JnzOkUeGRihdVE')
         const tree = renderer
             .create(<BrowserRouter><NewResourceForm/></BrowserRouter>)
             .toJSON()
